206871d70500d6d45e4aa18ac0582d54e2060d4f
rotemge <rotemgera@gmail.com>
2019-12-11 21:42:05 +0200

    DetailsActivity - play trailer on click

--- app/src/main/java/com/android/academy/DetailsActivity.kt
+++ app/src/main/java/com/android/academy/DetailsActivity.kt
@@ -1,7 +1,10 @@
 package com.android.academy
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -9,4 +12,12 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
     }
+
+    fun watchTrailer(button: View) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=6ZfuNTqbHE8"))
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
 }
